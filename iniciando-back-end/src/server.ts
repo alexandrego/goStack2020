ac9dd2676ff8e33fbb4c26750b728b4a73731c40
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2023-05-15 21:35:39 -0300

    chore: Backup

---
+++ iniciando-back-end/src/server.ts
@@ -0,0 +1,17 @@
+import express, { application } from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+// app.get('/', (req, res) => {
+//  return res.json({ message: 'Fabiana Gonçalves' });
+// })
+
+app.listen(3333, () => {
+  console.log('🌍 Online ✔');
+});
